7228d69a0b0eb0b8668ee73cb048032dcf90b428
Sinan ISLEKDEMIR <sinan.islekdemir@foodie.fm>
2018-08-30 11:18:17 +0300

    Build Fixes

--- bbee/setup.py
+++ bbee/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bbee",
-    version="0.0.3",
+    version="0.0.4",
     author="Sinan Islekdemir",
     author_email="sinan@islekdemir.com",
     scripts=['bin/bbee'],
