29947f91535ae5872b4d8fb35634e0fd891cf710
Sinan ISLEKDEMIR <sinan.islekdemir@foodie.fm>
2018-09-15 12:25:17 +0300

    Bug fix and framework only on darwin

--- bbee/setup.py
+++ bbee/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bbee",
-    version="0.0.6",
+    version="0.0.7",
     author="Sinan Islekdemir",
     author_email="sinan@islekdemir.com",
     scripts=['bin/bbee'],
