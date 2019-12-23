6eb272af1c4af41d7c9bb0b296fe42cc04887a1f
sinan islekdemir <sinan@islekdemir.com>
2019-12-23 10:16:35 +0200

    Major clean-up

--- bbee/setup.py
+++ bbee/setup.py
@@ -5,15 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bbee",
-    version="0.0.9",
+    version="0.1.0",
     author="Sinan Islekdemir",
     author_email="sinan@islekdemir.com",
-    scripts=['bin/bbee'],
+    entry_points={
+        'console_scripts': [
+            'bbee = bbee:main',
+        ]
+    },
     description="A small C/C++ Builder",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sinanislekdemir/bbee",
     packages=setuptools.find_packages(),
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
